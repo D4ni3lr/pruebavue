9a775123565b81acf5283e207d4adb24b70533e5
D4ni3lr <hectordrt@hotmail.com>
2017-11-14 23:27:22 -0500

    Primer commit

---
+++ src/components/PrimerComponente.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="primerComponente">
+        Valor de la propiedad: {{parametro}}
+        <input type="text" v-bind:value="parametro" @input="$emit('update:parametro', $event.target.value)">
+    </div>
+</template>
+<script>
+  export default {
+    name: 'primer-componente',
+    data () {
+      return {
+        data: ''
+      }
+    },
+    mounted: function () {
+      this.data = this.parametro
+    },
+    props: ['parametro']
+  }
+</script>
+<style>
+    .primerComponente {
+        color: blue;
+    }
+</style>
---
+++ src/components/intro.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="intro">
+        <span>Intro</span>
+        <span>{{message}}</span>
+        <span>Bind del titulo</span>
+        <span v-bind:title="message2">Este texto debe mostrar un mensaje con fecha</span>
+        <span v-if="mostrarMsj">Texto condicional, debe desaparecer cuando veamos eventos</span>
+        <span>Ciclos, siguiente lista de datos:</span>
+        <ol>
+            <li v-for="dato in lista">
+                {{dato}}
+            </li>
+        </ol>
+        <span>eventos</span>
+        <button v-on:click="mostrarOcultar">Mostrar/Ocultar</button>
+        <span>Cajas de texto</span>
+        <span>Valor en caja: {{input}}</span>
+        <input type="text" v-model="input">
+    </div>
+</template>
+<script>
+  export default {
+    name: 'intro',
+    data () {
+      return {
+        message: 'Hola mundo vue',
+        message2: 'Texto con fecha: ' + new Date().toLocaleString(),
+        mostrarMsj: true,
+        lista: [1, 2, 3, 8, 5, 658, 5, 8, 65, 6, 8, 9],
+        input: 'No inicializado'
+      }
+    },
+    methods: {
+      mostrarOcultar: function () {
+        this.mostrarMsj = !this.mostrarMsj
+      }
+    }
+  }
+</script>
+<style>
+    span {
+        display: block;
+    }
+</style>
