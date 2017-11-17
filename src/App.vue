7768da09bb55aa4136a3d14bf4f99f28fe2344c1
Daniel Ramirez <daniel.ramirez@geniar.net>
2017-11-17 16:49:09 -0500

    Se implementa un ejemplo de navbar responsive pero se debe ajustar.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,61 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        <!--
+        <img src="./assets/logo.png">
+        -->
+        <router-view/>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    /*
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+*/
+
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
+    body{
+        font-family: 'Open Sans', sans-serif;
+        margin-bottom: 60px;
+    }
+
+    .ui.navbar {
+        margin-top: 20px !important;
+    }
+
+    .padding-reset{
+        padding: 0px !important;
+    }
+
+    .ui.message{
+        padding: 40px !important;
+    }
+
+    @media (max-width: 767px) {
+        .ui.inverted.navbar{
+        }
+    }
+
+    .ui.vertical.menu{
+        margin-top: -15px !important;
+        width: 100%;
+        display: none;
+    }
+
 </style>
---
+++ src/components/SemanticShowcase/SemanticShowcase.vue
@@ -0,0 +1,12 @@
+<template src="./SemanticShowcase.html"></template>
+<script>
+  export default {
+    data () {
+      return {}
+    }
+  }
+
+
+  //ver este ejemplo: http://ehkoo.github.io/semantic-ui-examples/navbar/
+</script>
+
