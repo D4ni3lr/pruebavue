42b4f301fa78a8c232620c22c8c53c356e5d862d
D4ni3lr <hectordrt@hotmail.com>
2017-11-18 01:47:10 -0500

    Expose loader no expone la variable jQuery en la window pero si expone el $, por lo tanto no es necesario hacer reqire('jquery') ni tampoco import. Se envia demo de navbar responsive

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -80,9 +80,6 @@ module.exports = {
         test: require.resolve('jquery'),
         use: [{
           loader: 'expose-loader',
-          options: 'jQuery'
-        },{
-          loader: 'expose-loader',
           options: '$'
         }]
       }
--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@
 </script>
 
 <style>
-    /*
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -23,39 +22,4 @@
     color: #2c3e50;
     margin-top: 60px;
   }
-*/
-
-    *{
-        margin: 0;
-        padding: 0;
-    }
-
-    body{
-        font-family: 'Open Sans', sans-serif;
-        margin-bottom: 60px;
-    }
-
-    .ui.navbar {
-        margin-top: 20px !important;
-    }
-
-    .padding-reset{
-        padding: 0px !important;
-    }
-
-    .ui.message{
-        padding: 40px !important;
-    }
-
-    @media (max-width: 767px) {
-        .ui.inverted.navbar{
-        }
-    }
-
-    .ui.vertical.menu{
-        margin-top: -15px !important;
-        width: 100%;
-        display: none;
-    }
-
 </style>
